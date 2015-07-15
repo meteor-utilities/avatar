bbe3ba1ff401394b03e7809cc6a13fef1b1d2e06
Timothy Armes <tim@timothyarmes.com>
2015-07-15 13:54:51 +0200

    Allow user to define avatar sizes

--- helpers.js
+++ helpers.js
@@ -51,3 +51,87 @@ getEmailOrHash = function (user) {
   }
   return emailOrHash;
 };
+
+// Creates the dynaically generated CSS file
+//
+// CSS is dynamically generated so that we can have both a custom class prefix and also allow for custom sizes
+createCSS = function () {
+
+  // We only need to do this on the server
+
+  if (!Meteor.isServer)
+    return;
+
+  // The base CSS styles
+
+  var p = '.' + Avatar.getCssClassPrefix();
+  var a = p + ' ';
+
+  var css = 
+    p + ' { \n\
+      height: 50px; \n\
+      width: 50px; \n\
+      position: relative; \n\
+    } \n' + 
+    a + p + '-image, \n' +
+    a + p + '-initials { \n\
+      height: 100%; \n\
+      width: 100%; \n\
+      position: absolute; \n\
+      top: 0px; \n\
+      left: 0px; \n\
+    } \n' + 
+    a + p + '-image { \n\
+      z-index: 10; \n\
+      background-color: #fff; \n\
+    } \n' + 
+    a + p + '-initials { \n\
+      display: block; \n\
+      background-size: 100% 100%; \n\
+      background-color: #aaa; \n\
+      color: #fff; \n\
+      font-size: 25px; \n\
+      line-height: 50px; \n\
+      font-family: "Helvetica Neue", Helvetica, "Hiragino Sans GB", Arial, sans-serif; \n\
+      text-align: center; \n\
+      z-index: 1; \n\
+    } \n' + 
+    p + '-rounded ' + p + '-image, \n' +
+    p + '-rounded ' + p + '-initials { \n\
+      border-radius: 5px; \n\
+    } \n'+
+    p + '-circle ' + p + '-image, \n' +
+    p + '-circle ' + p + '-initials { \n\
+      border-radius: 50%; \n\
+    } \n' + 
+    p + '-hide-image ' + p + '-image { \n\
+      display: none; \n\
+    } \n' + 
+    p + '-hide-initials ' + p + '-initials { \n\
+      display: none; \n\
+    } \n\
+  ';
+
+  // CSS for each of the defined sizes
+
+  for (sizeName in Avatar.options.imageSizes) {
+  
+    var size = Avatar.options.imageSizes[sizeName];
+    
+    css = css + p + '-' + sizeName + ' {' +
+      'width: ' + size + 'px; ' + 
+      'min-width: ' + size + 'px; ' + 
+      'height: ' + size + 'px;' +
+    '}\n' +
+    p + '-' + sizeName + ' ' + p + '-initials {' +
+      'font-size: ' + size / 2 + 'px; ' +
+      'line-height: ' + size + 'px;' +
+    '}\n';
+  }
+
+  // In order to allow for custom sizes and a custom prefix we need to be able to create a style sheet
+  // on the fly. To do this cleanly we inject the styles directly into the HTML code before it's sent
+  // to the client using the meteor-hacks:inject package
+
+  Inject.rawHead('avatar-styles', '<style>' + css + '</style>');
+}
